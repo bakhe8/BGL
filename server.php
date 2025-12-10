1d234b4f64a629ca873c6179a3d6b9119f657725
Bakheet Ahmed Al-Zahrani <bakheet@gmail.com>
2025-12-10 12:35:29 +0300

    chore: unify UI across pages

--- server.php
+++ server.php
@@ -3,8 +3,25 @@
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $file = __DIR__ . '/www' . $uri;
 
+// Serve static assets manually from www/ so that docroot doesn't need to be changed
 if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
-    return false; // serve the static file
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    $types = [
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'svg' => 'image/svg+xml',
+        'ico' => 'image/x-icon',
+        'woff' => 'font/woff',
+        'woff2' => 'font/woff2',
+    ];
+    if (isset($types[$ext])) {
+        header('Content-Type: ' . $types[$ext]);
+    }
+    readfile($file);
+    exit;
 }
 
 require __DIR__ . '/www/index.php';
